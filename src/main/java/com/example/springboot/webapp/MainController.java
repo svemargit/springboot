cfa6374bec52d2c3178dd40b647a12c260a9a4da
svemargit <svejdam@icloud.com>
2023-08-11 14:00:28 +0200

    resolve dependency, add support for web, add first html page

---
+++ src/main/java/com/example/springboot/webapp/MainController.java
@@ -0,0 +1,13 @@
+package com.example.springboot.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+  @GetMapping("")
+  public String showHomePage() {
+    return "index";
+  }
+}
